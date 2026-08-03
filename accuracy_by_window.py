d0727004e4684262bf1de2ebe5c9149e1b60043a
kjungwoo5 <kjungwoo5@gmail.com>
2026-08-03 19:26:11 +0100

    More informative axes on accuracy by window

--- accuracy_by_window.py
+++ accuracy_by_window.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import matplotlib.ticker as mtick
+
 SESSION_PATH = Path(r"X:\Dammy\Xdetection_mouse_hf_test\session_topology_transitive_inference_cleaned.csv")
 HOME_PATH = Path(r"C:\bonsai\data\JungWoo")
 OUTPUT_PATH = Path(r"C:\Users\kjung\Documents\UCL\Year 4\ANAT0021 Dissertation\Coding\Analysis\Outputs")
@@ -15,7 +17,11 @@ type_of_plot = {
     1: 'Second position',
     2: 'Third position',
     3: 'Fourth position',
-    4: 'Stimulus Identity'
+    4: 'Stimulus Identity',
+    5: 'C-remaining sequence',
+    6: 'D-remaining sequence',
+    7: 'E-remaining sequence',
+    8: 'F-remaining sequence',
 }
 
 for a in range(1,5):
@@ -25,10 +31,10 @@ for a in range(1,5):
 
     for i, value in enumerate(type_of_plot.values()):
         #create scatterplot
-        plt.scatter(df.index, df[i])
+        plt.scatter(df.index*1000, df[i])
 
         #calculate equation for quadratic trendline
-        z = np.polyfit(df.index, df[i], 2)
+        z = np.polyfit(df.index*1000, df[i], 2)
         p_quad = np.poly1d(z)
         print(p_quad)
         p = np.poly1d(z)
@@ -37,13 +43,16 @@ for a in range(1,5):
 
         equation = str(round(z[0],2)) + "x**2 + " + str(round(z[1],2)) + "x + " + str(round(z[2],2)) 
         plt.plot(
-            df.index,
-            p_quad(df.index),
-            color='red', linestyle='--', label=f'Quadratic Trendline: {equation}'
+            df.index*1000,
+            p_quad(df.index*1000),
+            color='red', linestyle='--', label=f'Quadratic Trendline'
         )
         print(equation)
         plt.legend()
         plt.title(f'Decoding accuracy of {type_of_plot.get(i)} by window size for JK0{a}')
+        plt.xlabel('Window size (ms)')
+        plt.ylabel('Decoding accuracy')
+        plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax = 1, decimals = 1))
 
         fig = plt.gcf()
         fig.savefig(fr'{OUTPUT_PATH}\Accuracies by window size\JK0{a}\Stage5_JK0{a}_{type_of_plot.get(i)}_accuracies.svg')
